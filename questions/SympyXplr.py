ccb5d77493b43c6ff867ba22d961c7cfd2f13cfa
wilcrofter <bill@crofter.net>
2014-10-12 11:40:02 -0400

    first attempts to incorporate sympy.

---
+++ questions/SympyXplr.py
@@ -0,0 +1,28 @@
+from swirlypy.questions.Recording import *
+from sympy import *
+
+class SympyXplrQuestion(RecordingQuestion):
+     
+    def test_response(self, response, data={}):
+        """
+        just prints responses and returns True.
+        """
+        if "ast" in response:
+            print("\nast: ", ast.dump(response["ast"]))
+        if "added" in response:
+            print("\nadded: ", response["added"])
+        if "changed" in response:
+            print("changed: ", response["changed"])
+        if "removed" in response:
+            print("removed: ", response["removed"])
+        if "values" in response:
+            print("\nvalues: ", response["values"])
+        if "traceback" in response:
+            print("\ntraceback: ", response["traceback"])
+        else:
+            return response["traceback"] == None
+        
+    def execute(self, data={}):
+        super().execute(data=data)
+        print("coursedir: ", data["coursedir"])
+ 
